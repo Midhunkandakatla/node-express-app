71733b02c53a8ca7d9b6dc5d2c880dacffaa7b26
Midhunkandakatla <midhunkandakatla143@gmail.com>
2019-03-08 16:51:19 -0600

    first commit

--- app.js
+++ app.js
@@ -5,15 +5,24 @@ const hostname = '0.0.0.0'    // allows access from remote computers
 const port = 3002
 
 app.get('/', function (req, res) {
-  res.send('Welcome home!')
+  res.send('WEB APPS!')
 })
 
-app.get('/hello', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/big',  (req, res) =>{
   res.send('<h1>Hello World!</h1>')
+})
+app.get('/world',  (req, res) =>{
+  res.send('<h2> World is awesome!</h2>')
+})
+app.get('/earth',  (req, res) =>{
+  res.send('<h3>Earth is the only planet with human beings!</h3>')
+})
+app.get('/sun',  (req, res) =>{
+  res.send('<h4>Sun rises in the east and sets in the west!</h4>')
 })
 
 app.get('/greeting/:id',  (req, res) =>{
@@ -30,7 +39,7 @@ app.use((req, res, next) => {
 })
 
 app.listen(port, hostname, () => {
-  console.log(`Example app listening at http://${hostname}:${port}/`)
+  console.log(`Example app listening at http://localhost:${port}/`)
   console.log('Hit CTRL-C CTRL-C to stop\n')
 })
 
